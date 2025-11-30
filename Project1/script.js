3b5abb84500e25d572fe1d07cdd5f631d0b5da86
Dipayan Khaidem <dipayansingha8@gmail.com>
2025-12-01 00:18:54 +0530

    Snake movement and food consumption added

--- Project1/script.js
+++ Project1/script.js
@@ -4,11 +4,13 @@ const blockWidth=50
 
 const cols=Math.floor(board.clientWidth/blockWidth);
 const rows=Math.floor(board.clientHeight/blockHeight);
+let intervalId=null;
+let food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)}
 
 const blocks=[];
-const snake=[{x:1,y:3},
-    {x:1,y:4},
-    {x:1,y:5}];
+const snake=[{x:1,y:3}];
+
+let direction='down';
  
 // for(let i=0;i<rows*cols;i++){
 //     const block=document.createElement('div');
@@ -27,7 +29,52 @@ for(let row=0;row<rows;row++){
 }
 
 function render(){
+    let head=null;
+
+    blocks[`${food.x}-${food.y}`].classList.add("food")
+
+    if(direction==="left"){
+        head={x:snake[0].x,y:snake[0].y-1}   
+    }else if(direction==="right"){
+        head={x:snake[0].x,y:snake[0].y+1}
+    }else if(direction==="down"){
+        head={x:snake[0].x+1,y:snake[0].y}
+    }else if(direction=="up"){
+        head={x:snake[0].x-1,y:snake[0].y}
+    }
+
+    if(head.x<0 ||head.x>=rows || head.y<0 || head.y>=cols){
+        alert("Game Over");
+        clearInterval(intervalId);
+    }
+
+    if(head.x==food.x && head.y==food.y){
+        blocks[`${food.x}-${food.y}`].classList.remove("food")
+        food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)}
+        blocks[`${food.x}-${food.y}`].classList.add("food")
+    }
+
+    snake.forEach(segment=>{
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+    })
+    snake.unshift(head);
+    snake.pop();
     snake.forEach(segment=>{
         blocks[`${segment.x}-${segment.y}`].classList.add("fill");
     })
 }
+intervalId=setInterval(()=>{
+render();
+},300);
+
+addEventListener("keydown",(event)=>{
+    if(event.key==="ArrowUp"){
+        direction="up"
+    }else if(event.key==="ArrowDown"){
+        direction="down"
+    }else if(event.key==="ArrowLeft"){
+        direction="left"
+    }else if(event.key==="ArrowRight"){
+        direction="right"
+    }
+})
