b5278722d54fbbe599dc8e3aade3bd73f78e314f
Dipayan Khaidem <dipayansingha8@gmail.com>
2025-12-01 20:28:33 +0530

    added modals for game start and restart

--- Project1/script.js
+++ Project1/script.js
@@ -1,4 +1,10 @@
 const board=document.querySelector('.board');
+const startButton=document.querySelector('.btn-start');
+const modal=document.querySelector(".modal");
+const startGameModal=document.querySelector(".start-game");
+const gameOverModal=document.querySelector(".game-over");
+const restartButton=document.querySelector(".btn-restart");
+
 const blockHeight=50
 const blockWidth=50
 
@@ -8,7 +14,7 @@ let intervalId=null;
 let food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)}
 
 const blocks=[];
-const snake=[{x:1,y:3}];
+let snake=[{x:1,y:3}];
 
 let direction='down';
  
@@ -44,14 +50,21 @@ function render(){
     }
 
     if(head.x<0 ||head.x>=rows || head.y<0 || head.y>=cols){
-        alert("Game Over");
+        // alert("Game Over");
         clearInterval(intervalId);
+
+        modal.style.display="flex";
+        startGameModal.style.display="none";
+        gameOverModal.style.display="flex";
+        return;
     }
 
     if(head.x==food.x && head.y==food.y){
         blocks[`${food.x}-${food.y}`].classList.remove("food")
         food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)}
         blocks[`${food.x}-${food.y}`].classList.add("food")
+
+        snake.unshift(head);
     }
 
     snake.forEach(segment=>{
@@ -63,9 +76,24 @@ function render(){
         blocks[`${segment.x}-${segment.y}`].classList.add("fill");
     })
 }
-intervalId=setInterval(()=>{
-render();
-},300);
+// intervalId=setInterval(()=>{
+// render();
+// },300);
+
+startButton.addEventListener("click",()=>{
+    modal.style.display="none";
+    intervalId=setInterval(()=>{render()},300)
+})
+
+restartButton.addEventListener("click",restartGame)
+
+function restartGame(){
+modal.style.display="none";
+snake=[{x:1,y:3}];
+food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)}
+intervalId=setInterval(()=>{render()},300)
+
+}
 
 addEventListener("keydown",(event)=>{
     if(event.key==="ArrowUp"){
